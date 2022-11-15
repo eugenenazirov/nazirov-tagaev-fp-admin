ae1af1fa5e40a69ad0db5174eeafb6d615760499
Eugene Nazirov <jagzonmusic@gmail.com>
2022-11-15 23:46:05 +0600

    Comment the foreign key for future m2m refactoring

--- database/migrations/2022_11_15_132308_create_messages_table.php
+++ database/migrations/2022_11_15_132308_create_messages_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->text('msg_text');
-            $table->foreign('user_chat_id')->references('chat_id')->on('users');
+            // $table->foreign('user_chat_id')->references('chat_id')->on('users');
             $table->dateTime('created_at')->useCurrent();
         });
     }

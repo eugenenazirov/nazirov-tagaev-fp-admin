9d87e4a90aca5cf926c6d4690e46705213cc56df
Eugene Nazirov <jagzonmusic@gmail.com>
2022-11-15 19:29:41 +0600

    Create migrations

---
+++ database/migrations/2022_11_15_130738_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * The database connection that should be used by the migration.
+     *
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->char('name', 255);
+            $table->integer('telegram_id');
+            $table->char('first_name', 255);
+            $table->char('last_name', 255);
+            $table->integer('chat_id');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at');
+            $table->dateTime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ database/migrations/2022_11_15_132308_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->text('msg_text');
+            $table->foreign('user_chat_id')->references('chat_id')->on('users');
+            $table->dateTime('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};

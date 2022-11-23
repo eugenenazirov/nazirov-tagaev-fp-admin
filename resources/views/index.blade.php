08d4146cf2eae25e887d108105d4204415ed78ca
Eugene Nazirov <jagzonmusic@gmail.com>
2022-11-23 20:40:22 +0600

    Refactor textarea

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Page</title>
+    <title>Admin Panel - MailingsBot</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
@@ -16,6 +16,9 @@
 
             <div class="main__messages" id="messages">
                 <ul class="messages__items">
+                    @if (!$messages)
+                        <p class="messages__no-messages">Нет сообщений!</p>
+                    @endif
                     @foreach ($messages as $message)
                         <li class="message-item">
                             <span class="message-item__time">{{ $message->created_at }}</span>{{ $message->msg_text }}</li>
@@ -24,7 +27,7 @@
             </div>
             
             <form action="/send" method="post" class="main__form">
-                <textarea id="textInput" type="text" class="form__input" name="message"></textarea>
+                <textarea id="textInput" type="text" class="form__input" name="message" placeholder="Ваше сообщение..."></textarea>
                 <button id="btn" type="submit" class="btn form__btn">
                     Send Message
                 </button>
